a5857f2ec8461622b1aef78c1d87aae11f69dfb2
mehtasoham214 <mehtasoham214@gmail.com>
2023-02-26 18:23:44 -0500

    Added the base code for home and updated the Files

--- solar_management_frontend/src/component/App.js
+++ solar_management_frontend/src/component/App.js
@@ -1,5 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+//Import Solar Management pages
+import home from "./home";
+import login from "./login";
+
+//Import Common components
+import header from "./common/header";
+import navigation from "./common/navigation";
 
 function App() {
   return (
