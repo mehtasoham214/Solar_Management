207854fa5571ed0adb10e31b2570e6201cfd248b
dhriti24 <shah.dhriti@gmail.com>
2023-02-26 18:22:35 -0500

    database connections

---
+++ solar_management_backend/db/collection.js
@@ -0,0 +1,20 @@
+const dbConnection = require("./conn.js");
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection.dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+module.exports = {
+  user: getCollectionFn("user"),
+  customer: getCollectionFn("customer"),
+  project: getCollectionFn("project"),
+};
