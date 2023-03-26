5bf9335a7c71e9f99642c23645dc6ece92f77590
mehtasoham214 <mehtasoham214@gmail.com>
2023-03-26 15:45:19 -0400

    Firebase Databse

--- solar_management_frontend/src/components/Firebase/firebase.js
+++ solar_management_frontend/src/components/Firebase/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref } from "firebase/database";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
@@ -19,6 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getFirestore(app);
 
-export { database, ref };
+export { db };
