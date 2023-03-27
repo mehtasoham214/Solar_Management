e47b142a5d5442c9d6aa91a8081c6a779f82eea8
Aditya Gupta <kuku.g.r@gmail.com>
2023-03-27 02:16:06 -0400

    made changes in siteinspector form file

--- solar_management_frontend/src/App.js
+++ solar_management_frontend/src/App.js
@@ -1,23 +1,22 @@
 import "./App.css";
 import React from "react";
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/salesDashboard/dashboard";
 import ALLOngoingProjects from "./components/onGoingProjects";
 import ALLPastProjects from "./components/pastProjects";
 import ProjectDashboard from "./components/salesDashboard/projectDashboard";
 import SiteInspectorForm from "./components/SiteInspectorForm";
-=======
-import { BrowserRouter as Router} from "react-router-dom";
+
+// import { BrowserRouter as Router} from "react-router-dom";
 
 import SalesRoutes from "./Routes";
->>>>>>> a8188732002495188bcd6a282b7424c674b8b23e
 
 
 function App() {
     return (
         <Router>
-<<<<<<< HEAD
+
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route
@@ -28,12 +27,12 @@ function App() {
                 <Route path="/projectdetails" element={<ProjectDashboard />} />
                 <Route path="/SiteInspectorForm" element={<SiteInspectorForm />} />
             </Routes>
-=======
+
             <SalesRoutes />
->>>>>>> a8188732002495188bcd6a282b7424c674b8b23e
-        </Router>
-       
-    );
+
+
+ 
+            </Router>  );
 }
 
 export default App;
--- solar_management_frontend/src/components/SiteInspectorForm/index.js
+++ solar_management_frontend/src/components/SiteInspectorForm/index.js
@@ -6,126 +6,217 @@ import { ThemeProvider } from "@mui/material/styles";
 import { Box, Container } from "@mui/system";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import { Button } from "@mui/material";
 import Send from '@mui/icons-material/Send';
-import { useState } from "react";
-  
+import { useState } from "react"; 
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import RadioGroup from "@mui/material";
 import Radio from "@mui/material";
 import FormLabel from "@mui/material";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import Radio from '@mui/material/Radio';
+import Button from '@mui/material/Button'
 
 
 
+export default function SiteInspectorForm() {
+  return (
+    
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField helperText="Sqft" id="outlined-basic" label="Measurements" variant="outlined" />
+      <div>
+      <FormControl>
+  <FormLabel id="demo-radio-buttons-group-label">Meter Compatibility</FormLabel>
+  <RadioGroup
+  row
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="Yes"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="female" control={<Radio />} label="Yes" />
+    <FormControlLabel value="male" control={<Radio />} label="No" />
+    
+  </RadioGroup>
+</FormControl>
+</div>
+<div>
+<TextField helperText="Watts/sqft"id="outlined-basic" label="Irradiance" variant="outlined" />
 
-function SiteInspectorForm()
-{
-  const [checked, setChecked] = React.useState([true, false]);
-
-  const handleChange1 = (event) => {
-    setChecked([event.target.checked, event.target.checked]);
-  };
-
-  const handleChange2 = (event) => {
-    setChecked([event.target.checked, checked[1]]);
-  };
-
-  const handleChange3 = (event) => {
-    setChecked([checked[0], event.target.checked]);
-  };
-
-  const [formData, setFormData] = useState({
-    Measurements: '',
-    email: '',
-    message: ''
-
-  });
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
-  };  
+  </div>
   
-  const handleFormChange = (event) => {
-    const { Measurements, value } = event.target;
-    setFormData({ ...formData, [Measurements]: value });
-  };
-  const children = (
-    <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
-      <FormControlLabel
-        label="Child 1"
-        control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
-      />
-      <FormControlLabel
-        label="Child 2"
-        control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
-      />
+  <div>
+  <FormControl>
+    <FormLabel
+      sx={{ display: "inline-flex" }}
+      id="demo-radio-buttons-group-label"
+      value="start"
+    >
+      Feasibility
+    </FormLabel>
+    <RadioGroup
+      sx={{ display: "flex", flexDirection: "row" }}
+      aria-labelledby="demo-radio-buttons-group-label"
+      defaultValue="Yes"
+      name="radio-buttons-group"
+    >
+      <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+      <FormControlLabel value="No" control={<Radio />} label="No" />
+    </RadioGroup>
+  </FormControl>
+</div>
+
+
+
+  <div>
+  <FormControl>
+  <FormLabel id="demo-radio-buttons-group-label">Structural Feasibility</FormLabel>
+  <RadioGroup
+  row
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="Yes"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="female" control={<Radio />} label="Yes" />
+    <FormControlLabel value="male" control={<Radio />} label="No" />
+    
+  </RadioGroup>
+</FormControl>
+  </div>
+ 
+<div>
+
+<Button  variant="contained">Upload Photos</Button>
+
+  </div>
+      
+      
+      
+      
     </Box>
   );
-  return (
-    <form onSubmit={handleFormSubmit}>
+}
 
-    <div style={{ display: 'block' }}>
-      <TextField
-        id="Measurements"
-        Measurements="Measurements"
-        label="Measurements"
-        variant="outlined"
-        value={formData.name}
-        onChange={handleFormChange}
-      />
-    </div>
+
+
+// function SiteInspectorForm()
+// {
+//   const [checked, setChecked] = React.useState([true, false]);
+
+//   const handleChange1 = (event) => {
+//     setChecked([event.target.checked, event.target.checked]);
+//   };
+
+//   const handleChange2 = (event) => {
+//     setChecked([event.target.checked, checked[1]]);
+//   };
+
+//   const handleChange3 = (event) => {
+//     setChecked([checked[0], event.target.checked]);
+//   };
+
+//   const [formData, setFormData] = useState({
+//     Measurements: '',
+//     email: '',
+//     message: ''
+
+//   });
+
+//   const handleFormSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(formData);
+//   };  
   
-    <div style={{ display: 'block' }}>
-      <TextField
-        id="email"
-        name="email"
-        label="Email"
-        variant="outlined"
-        value={formData.email}
-        onChange={handleFormChange}
-      />
-    </div>
-    <div style={{ display: 'block' }}>
-      <TextField
-        id="message"
-        name="message"
-        label="Message"
-        variant="outlined"
-        multiline
-        rows={4}
-        value={formData.message}
-        onChange={handleFormChange}
-      />
-    </div>
-    <div style={{ display: 'block' }}>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        endIcon={<Send />}
-      >
-        Send
-      </Button>
-    </div>
-    <div>
-      <FormControlLabel
-        label="Parent"
-        control={
-          <Checkbox
-            checked={checked[0] && checked[1]}
-            indeterminate={checked[0] !== checked[1]}
-            onChange={handleChange1}
-          />
-        }
-      />
-      {children}
-    </div>
-  </form>
+//   const handleFormChange = (event) => {
+//     const { Measurements, value } = event.target;
+//     setFormData({ ...formData, [Measurements]: value });
+//   };
+//   const children = (
+//     <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
+//       <FormControlLabel
+//         label="Child 1"
+//         control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
+//       />
+//       <FormControlLabel
+//         label="Child 2"
+//         control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+//       />
+//     </Box>
+//   );
+//   return (
+//     <form onSubmit={handleFormSubmit}>
+
+//     <div style={{ display: 'block' }}>
+//       <TextField
+//         id="Measurements"
+//         Measurements="Measurements"
+//         label="Measurements"
+//         variant="outlined"
+//         value={formData.name}
+//         onChange={handleFormChange}
+//       />
+//     </div>
+  
+//     <div style={{ display: 'block' }}>
+//       <TextField
+//         id="email"
+//         name="email"
+//         label="Email"
+//         variant="outlined"
+//         value={formData.email}
+//         onChange={handleFormChange}
+//       />
+//     </div>
+//     <div style={{ display: 'block' }}>
+//       <TextField
+//         id="message"
+//         name="message"
+//         label="Message"
+//         variant="outlined"
+//         multiline
+//         rows={4}
+//         value={formData.message}
+//         onChange={handleFormChange}
+//       />
+//     </div>
+//     <div style={{ display: 'block' }}>
+//       <Button
+//         type="submit"
+//         variant="contained"
+//         color="primary"
+//         endIcon={<Send />}
+//       >
+//         Send
+//       </Button>
+//     </div>
+//     <div>
+//       <FormControlLabel
+//         label="Parent"
+//         control={
+//           <Checkbox
+//             checked={checked[0] && checked[1]}
+//             indeterminate={checked[0] !== checked[1]}
+//             onChange={handleChange1}
+//           />
+//         }
+//       />
+//       {children}
+//     </div>
+//   </form>
     
 
 //   <form onSubmit={handleFormSubmit}>
@@ -165,7 +256,6 @@ function SiteInspectorForm()
 //   </Button>
 // </form>
 
-  )
-}
+ 
 
-export default SiteInspectorForm;
+
