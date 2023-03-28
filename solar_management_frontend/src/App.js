eeace567ffe543e79ce695d648de82862c37f6ce
dhriti24 <shah.dhriti@gmail.com>
2023-03-27 23:02:24 -0400

    modification to solve errors

--- solar_management_frontend/src/App.js
+++ solar_management_frontend/src/App.js
@@ -1,38 +1,16 @@
 import "./App.css";
 import React from "react";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./components/salesDashboard/dashboard";
-import ALLOngoingProjects from "./components/onGoingProjects";
-import ALLPastProjects from "./components/pastProjects";
-import ProjectDashboard from "./components/salesDashboard/projectDashboard";
-import SiteInspectorForm from "./components/SiteInspectorForm";
-
+import { BrowserRouter as Router} from "react-router-dom";
 // import { BrowserRouter as Router} from "react-router-dom";
-
-import SalesRoutes from "./Routes";
+import ProjectRouter from "./Routes";
 
 
 function App() {
     return (
         <Router>
-
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route
-                    path="/ongoingprojects"
-                    element={<ALLOngoingProjects />}
-                />
-                <Route path="/pastprojects" element={<ALLPastProjects />} />
-                <Route path="/projectdetails" element={<ProjectDashboard />} />
-                <Route path="/SiteInspectorForm" element={<SiteInspectorForm />} />
-            </Routes>
-
-            <SalesRoutes />
-
-
- 
-            </Router>  );
+            <ProjectRouter />
+        </Router>  );
 }
 
 export default App;
