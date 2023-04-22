64398735603f131ed8003066f4aab0d13b234a1d
Prudhvi Krishna Paruchuri <56634854+prudhvikrishnap@users.noreply.github.com>
2023-04-22 13:33:04 -0400

    Added code for OE to view SI data

--- solar_management_frontend/src/components/operationEngineerDashboard/projectDetails/siteInspectorInfo.js
+++ solar_management_frontend/src/components/operationEngineerDashboard/projectDetails/siteInspectorInfo.js
@@ -37,27 +37,28 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 function SiteInspectorInfo() {
-    const [customer, getcustomer] = useState();
+    const [sidata, getsidata] = useState();
 
     async function GetcustomerDetails() {
         const token = localStorage.getItem("token");
         const projectId = localStorage.getItem("projectId");
         const response = await axios.get(
-            `${process.env.REACT_APP_API_URL}customer/${projectId}`,
+            `${process.env.REACT_APP_API_URL}projects/${projectId}`,
             {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
             }
         );
-        const data = await response.data;
-        getcustomer(data);
+        const data = await response.data.areaInfo;
+        getsidata(data);
+        console.log(data)
     }
     useEffect(() => {
         GetcustomerDetails();
     }, []);
 
-    if (!customer) return <div>No Customer Found</div>;
+    if (!sidata) return <div>No Site Inspector Data Found</div>;
 
     return (
         <ThemeProvider theme={theme}>
@@ -86,10 +87,254 @@ function SiteInspectorInfo() {
                                             component="th"
                                             scope="row"
                                         >
-                                            Roof Area
+                                            Backyard Info
                                         </StyledTableCell>
                                         <StyledTableCell align="right">
-                                            500
+                                            {sidata.backyardInfo}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Roof Info
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">
+                                            {sidata.roofInfo}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Grid
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">
+                                            {sidata.grid}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Irradiance
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">
+                                            {sidata.irradiance}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Meter Compatibility
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.meterCompatibility === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.meterCompatibility ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.meterCompatibility}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Co-ordinates
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">
+                                            {sidata.coordinates}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Environmental Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                                color:
+                                                    sidata.environment === "Yes"
+                                                        ? theme.palette.success.main
+                                                        : sidata.environment ===
+                                                        "No"
+                                                        ? theme.palette.error.main
+                                                        : "",
+                                        }}>
+                                            {sidata.environment}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Building Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.building === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.building ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.building}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Electrical Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.electrical === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.electrical ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.electrical}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Zonal Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.zone === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.zone ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.zone}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Land Use Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.landUse === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.landUse ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.landUse}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Inter-Connection Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.interconnection === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.interconnection ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.interconnection}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Net Metering Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.netMetering === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.netMetering ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.netMetering}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            Property Easement Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.propertyEasement === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.propertyEasement ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.propertyEasement}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                    <StyledTableRow>
+                                        <StyledTableCell
+                                            component="th"
+                                            scope="row"
+                                        >
+                                            HOA Permit
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.hoa === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.hoa ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.hoa}
                                         </StyledTableCell>
                                     </StyledTableRow>
                                     <StyledTableRow>
@@ -99,8 +344,17 @@ function SiteInspectorInfo() {
                                         >
                                             Feasability
                                         </StyledTableCell>
-                                        <StyledTableCell align="right">
-                                            YES
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.feasibility === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.feasibility ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.feasibility}
                                         </StyledTableCell>
                                     </StyledTableRow>
                                     <StyledTableRow>
@@ -108,10 +362,19 @@ function SiteInspectorInfo() {
                                             component="th"
                                             scope="row"
                                         >
-                                            Sunlight Access
+                                            Structural Feasibility
                                         </StyledTableCell>
-                                        <StyledTableCell align="right">
-                                            Partially
+                                        <StyledTableCell align="right"
+                                        style={{
+                                            color:
+                                                sidata.structuralFeasibility === "Yes"
+                                                    ? theme.palette.success.main
+                                                    : sidata.structuralFeasibility ===
+                                                    "No"
+                                                    ? theme.palette.error.main
+                                                    : "",
+                                    }}>
+                                            {sidata.structuralFeasibility}
                                         </StyledTableCell>
                                     </StyledTableRow>
                                 </TableBody>
